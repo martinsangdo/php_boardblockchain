f68cf4cc7fac15d6d454ff6b4994b96b6e402efb
Martin SangDo <martin.sangdo@gmail.com>
2018-05-13 02:48:58 +0700

    - fill data to homepage

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -14,32 +14,6 @@ class Welcome extends MY_Controller
     }
 
     /**
-     * gamereviewnews
-     */
-    /*
-    public function index(){
-        //get data of blocks
-        $this->data[BLOCK_KEY_1] = $this->block_content_model->get_latest_posts(array('site_id' => 4), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_2] = $this->block_content_model->get_latest_posts(array('site_id' => 16), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_3] = $this->block_content_model->get_latest_posts(array('site_id' => 6), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_4] = $this->block_content_model->get_latest_posts(array('site_id' => 7), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_5] = $this->block_content_model->get_latest_posts(array('site_id' => 18), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_6] = $this->block_content_model->get_latest_posts(array('site_id' => 9), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_7] = $this->block_content_model->get_latest_posts(array('site_id' => 10), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_8] = $this->block_content_model->get_latest_posts(array('site_id' => 21), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_9] = $this->block_content_model->get_latest_posts(array('site_id' => 12), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_10] = $this->block_content_model->get_latest_posts(array('site_id' => 13), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_12] = $this->block_content_model->get_latest_posts(array('site_id' => 15), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_13] = $this->block_content_model->get_latest_posts(array('site_id' => 1), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_14] = $this->block_content_model->get_latest_posts(array('site_id' => 23), 0, DEFAULT_PAGE_LEN);
-        //get videos
-        $this->load->model('video_model');
-        $this->data['videos'] = $this->video_model->get_latest_posts(array('status' => 1), 0, DEFAULT_PAGE_LEN);
-
-        $this->load->view(VIEW_FOLDER.'/home', $this->data);
-    }
-    */
-    /**
      * blockbod
      */
     public function index(){
@@ -52,10 +26,12 @@ class Welcome extends MY_Controller
         $this->data[BLOCK_KEY_6] = $this->block_content_model->get_latest_posts(array('site_id' => 7), 0, DEFAULT_PAGE_LEN);
         $this->data[BLOCK_KEY_7] = $this->block_content_model->get_latest_posts(array('site_id' => 10), 0, DEFAULT_PAGE_LEN);
         $this->data[BLOCK_KEY_8] = $this->block_content_model->get_latest_posts(array('site_id' => 12), 0, DEFAULT_PAGE_LEN);
-        $this->data[BLOCK_KEY_9] = $this->block_content_model->get_latest_posts(array('site_id' => 17), 0, DEFAULT_PAGE_LEN);
+        $this->data[BLOCK_KEY_9] = $this->block_content_model->get_latest_posts(array('site_id' => 13), 0, DEFAULT_PAGE_LEN);
         $this->data[BLOCK_KEY_10] = $this->block_content_model->get_latest_posts(array('site_id' => 14), 0, DEFAULT_PAGE_LEN);
         $this->data[BLOCK_KEY_11] = $this->block_content_model->get_latest_posts(array('site_id' => 15), 0, DEFAULT_PAGE_LEN);
-//        $this->data[BLOCK_KEY_12] = $this->block_content_model->get_latest_posts(array('site_id' => 17), 0, DEFAULT_PAGE_LEN);
+        $this->data[BLOCK_KEY_12] = $this->block_content_model->get_latest_posts(array('site_id' => 16), 0, DEFAULT_PAGE_LEN);
+        $this->data[BLOCK_KEY_13] = $this->block_content_model->get_latest_posts(array('site_id' => 17), 0, DEFAULT_PAGE_LEN);
+        $this->data[BLOCK_KEY_14] = $this->block_content_model->get_latest_posts(array('site_id' => 18), 0, DEFAULT_PAGE_LEN);
 
         $this->load->view(VIEW_FOLDER.'/home', $this->data);
     }
